cf1ea0abeaf8da819806c5d6b66048f409cf7587
Manikandan <nmanikandan2211@gmail.com>
2022-08-19 07:30:51 +0530

    finished

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { TransactionProvider } from "./context/TransactionContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <TransactionProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </TransactionProvider>
+import App from "./App";
+import { TransactionsProvider } from "./context/TransactionContext";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <TransactionsProvider>
+    <App />
+  </TransactionsProvider>
 );
